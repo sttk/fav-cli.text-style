fa73456edc564872e2d6b32466f6ec3ecb8088ec
sttk <sttk.xslet@gmail.com>
2019-07-15 18:49:01 +0900

    Perf: Add benchmark

---
+++ benchmark/Benchmark.md
@@ -0,0 +1,17 @@
+# Benchmark of @fav/cli.text-style
+
+## v0.1.0
+
+Comparing with following modules:
+
+* [chalk](https://www.npmjs.com/package/chalk)
+* [ansi-colors](https://www.npmjs.com/package/ansi-colors))
+
+|        | @fav/cli.text-style(0.1.0) | chalk(2.4.2)  | ansi-colors(4.1.1) |
+|:-------|---------------------------:|--------------:|-------------------:|
+| Colors |            177,363 ops/sec | 4,756 ops/sec |    106,909 ops/sec |
+| Nested |             30,618 ops/sec | 4,532 ops/sec |     23,434 ops/sec |
+
+- Platform: Node.js 12.2.0 on Darwin 64-bit
+- Machine: Intel(R) Core(TM) i7-2620M CPU @ 2.70GHz, 16GB
+
---
+++ benchmark/benchmark.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const BenchmarkTester = require('benchmark-tester');
+
+const colors = [
+  'reset',
+  'bold',
+  'dim',
+  'italic',
+  'underline',
+  'inverse',
+  'hidden',
+  'strikethrough',
+  'black',
+  'red',
+  'green',
+  'yellow',
+  'blue',
+  'magenta',
+  'cyan',
+  'white',
+  'gray',
+  'bgBlack',
+  'bgRed',
+  'bgGreen',
+  'bgYellow',
+  'bgBlue',
+  'bgMagenta',
+  'bgCyan',
+];
+
+function testColors(styles, names) {
+  names.forEach(name => styles[name]('foo'));
+}
+
+function testNested(styles, names) {
+  names.reduce((result, name) => styles[name](result));
+}
+
+new BenchmarkTester()
+  .addTest('@fav/cli.text-style', testColors)
+  .addTest('chalk', testColors)
+  .addTest('ansi-colors', testColors)
+  .runTest('Colors', colors)
+
+  .addTest('@fav/cli.text-style', testNested)
+  .addTest('chalk', testNested)
+  .addTest('ansi-colors', testNested)
+  .runTest('Nested', colors)
+
+  .print();
